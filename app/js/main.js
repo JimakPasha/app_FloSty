ec86a18ffa83798d6aeb7db2fe440a323db1011f
JimakPasha <Jimak@tut.by>
2021-01-28 14:29:23 +0300

    start hit-product section

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,9 @@
 $(function () {
   
+  $('.hitpro-slider__items').slick({
+    dots: true,
+    prevArrow: '<button type="button" class="slick-prev"><img src="images/arrow-prev.svg" alt="назад"></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="images/arrow-next.svg" alt="вперёд"></button>',
+  });
+
 });
