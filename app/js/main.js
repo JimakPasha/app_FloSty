1d064ddef2cf3a8fe431398b4a48982d544030c9
JimakPasha <Jimak@tut.by>
2021-01-29 20:37:17 +0300

    added reviews and contacts section

--- app/js/main.js
+++ app/js/main.js
@@ -3,7 +3,16 @@ $(function () {
   $('.hitpro-slider__items').slick({
     dots: true,
     prevArrow: '<button type="button" class="slick-prev"><img src="images/arrow-prev.svg" alt="назад"></button>',
-    nextArrow: '<button type="button" class="slick-next"><img src="images/arrow-next.svg" alt="вперёд"></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="images/arrow-next.svg" alt="вперёд"></button>'
+  });
+
+
+  $('.reviews__inner').slick({
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    dots: true,
+    prevArrow: '<button type="button" class="slick-prev"><img src="images/arrow-prev.svg" alt="назад"></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="images/arrow-next.svg" alt="вперёд"></button>'
   });
 
 });
