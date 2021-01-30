a3b200831ae641dbd1bf669151dee352203f67f2
JimakPasha <Jimak@tut.by>
2021-01-31 01:11:59 +0300

    start shop page

--- app/js/main.js
+++ app/js/main.js
@@ -15,4 +15,21 @@ $(function () {
     nextArrow: '<button type="button" class="slick-next"><img src="images/arrow-next.svg" alt="вперёд"></button>'
   });
 
+
+  $('.filter__title').on('click', function () {
+    if($('.filter').hasClass('one')){
+      $('.filter__title').not($(this)).removeClass('active');
+      $('.filter__form').not($(this).next()).slideUp(250);
+    }
+    $(this).toggleClass('active').next().slideToggle(250);
+  });
+
+  $('.recommend__inner').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    dots: true,
+    prevArrow: '<button type="button" class="slick-prev"><img src="images/arrow-prev.svg" alt="назад"></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="images/arrow-next.svg" alt="вперёд"></button>'
+  });
+
 });
