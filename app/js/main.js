5a96080a60dba548c64f985b4bd5e2a17d692f2f
JimakPasha <Jimak@tut.by>
2021-02-18 00:48:41 +0300

    added product page

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,44 @@
 $(function () {
+
+  $('.description-slider__inner').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    dots: true,
+    prevArrow: '<button type="button" class="slick-prev"><img src="images/arrow-prev.svg" alt="назад"></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="images/arrow-next.svg" alt="вперёд"></button>'
+  });
+
+  $('.product-slide__thumb').slick({
+    asNavFor:'.product-slide__big',
+    focusOnSelect: true,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    vertical: true,
+    draggable: false
+  });
+  $('.product-slide__big').slick({
+    asNavFor: '.product-slide__thumb',
+    draggable: false,
+    arrows: false,
+    fade: true
+  });
+
+  //E-mail Ajax Send
+  $("form").submit(function () { //Change
+    var th = $(this);
+    $.ajax({
+      type: "POST",
+      url: "mail.php", //Change
+      data: th.serialize()
+    }).done(function () {
+      alert("Cпасибо! Мы свяжемся с вами:)");
+      setTimeout(function () {
+        // Done Functions
+        th.trigger("reset");
+      }, 1000);
+    });
+    return false;
+  });
   
   $('.hitpro-slider__items').slick({
     dots: true,
@@ -32,7 +72,7 @@ $(function () {
     nextArrow: '<button type="button" class="slick-next"><img src="images/arrow-next.svg" alt="вперёд"></button>'
   });
 
-  $('.shop-content__select').styler();
+  $('.shop-content__select, .product-one__num-input').styler();
 
   $('.similar__inner').slick({
     slidesToShow: 3,
@@ -42,4 +82,5 @@ $(function () {
     nextArrow: '<button type="button" class="slick-next"><img src="images/arrow-next.svg" alt="вперёд"></button>'
   });
 
+  
 });
